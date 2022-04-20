fcdaa484ba488cc2beb42fbc790a9951975e851a
dbaxter93 <drewbaxter93@outlook.com>
2022-04-19 22:03:09 -0500

    Add files via upload

---
+++ CrashDetector.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetector : MonoBehaviour
+{
+    [SerializeField] float loadDelay = 0.5f;
+    [SerializeField] ParticleSystem deathEffect;
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "Ground")
+        {
+            deathEffect.Play();
+            Invoke("ReloadScene", loadDelay);
+        }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
